061bf4e5bd69373fa45e69d30ac697099aaf1ed1
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-12-24 18:46:46 -0800

    app:next - landing page's hero section

--- apps/web/next/app/page.tsx
+++ apps/web/next/app/page.tsx
@@ -1,10 +1,11 @@
-
 import Navbar from "@/app/_components/navbar";
+import HeroSection from "@/app/_components/hero-section";
 
 export default function Home() {
   return (
     <main className="bg-background">
       <Navbar />
+      <HeroSection />
     </main>
   );
 }
