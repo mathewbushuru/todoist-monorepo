eda4f84999c57a561df07453e4cef9cfed6b9d8a
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-12-31 13:15:32 -0800

    app:webapp - side panel styling

--- apps/web/webapp/src/pages/home-page.tsx
+++ apps/web/webapp/src/pages/home-page.tsx
@@ -9,10 +9,10 @@ import {
   CalendarClock,
   CalendarDays,
   LayoutGrid,
-  SquareAsterisk
+  SquareAsterisk,
 } from "lucide-react";
 import { Drawer, DrawerTrigger, DrawerContent } from "ui";
-import { Button } from "ui";
+import { AppButton } from "@/components/app-button";
 
 function HomePage() {
   return (
@@ -21,53 +21,93 @@ function HomePage() {
         {/* Side panel  */}
         <nav className="bg-backgroundMuted hidden w-1/4 flex-col items-stretch gap-1 px-3 py-6 sm:flex">
           {/* Side panel header - Settings Dropdown button, notifications */}
-          <div className="text-xs h-12"></div>
+          <div className="h-12 text-xs"></div>
           {/* Side panel add task  */}
-          <Button variant="ghost" contentAlign="left" size="sm">
-            <PlusCircle className="h-5 w-5"/>
+          <AppButton
+            variant="ghost"
+            size="sm"
+            className="mb-2 justify-start text-primary hover:bg-primary/5"
+          >
+            <PlusCircle className="h-5 w-5" />
             Add task
-          </Button>
+          </AppButton>
 
           {/* Side panel filters  */}
-          <Button variant="ghost" contentAlign="left" size="sm">
-            <Search className="h-5 w-5"/>
+          <AppButton
+            variant="ghost"
+            size="sm"
+            className="justify-start font-light hover:bg-primary/5"
+          >
+            <Search className="h-4 w-4 text-muted/80" />
             Search
-          </Button>
-          <Button variant="ghost" contentAlign="left" size="sm">
-            <Inbox className="h-5 w-5"/>
+          </AppButton>
+          <AppButton
+            variant="ghost"
+            size="sm"
+            className="justify-start bg-primary/10 font-light text-primary hover:bg-primary/10"
+          >
+            <Inbox className="h-4 w-4 text-primary" />
             Inbox
-          </Button>
-          <Button variant="ghost" contentAlign="left" size="sm">
-            <CalendarClock className="h-5 w-5"/>
+          </AppButton>
+          <AppButton
+            variant="ghost"
+            size="sm"
+            className="justify-start font-light hover:bg-primary/5"
+          >
+            <CalendarClock className="h-4 w-4 text-muted/80" />
             Today
-          </Button>
-          <Button variant="ghost" contentAlign="left" size="sm">
-            <CalendarDays className="h-5 w-5"/>
+          </AppButton>
+          <AppButton
+            variant="ghost"
+            size="sm"
+            className="justify-start font-light hover:bg-primary/5"
+          >
+            <CalendarDays className="h-4 w-4 text-muted/80" />
             Upcoming
-          </Button>
-          <Button variant="ghost" contentAlign="left" size="sm">
-            <LayoutGrid className="h-5 w-5"/>
+          </AppButton>
+          <AppButton
+            variant="ghost"
+            size="sm"
+            className="justify-start font-light hover:bg-primary/5"
+          >
+            <LayoutGrid className="h-4 w-4 text-muted/80" />
             Filters & Labels
-          </Button>
+          </AppButton>
 
           {/* Side panel projects  */}
-          <h3 className="mt-4 font-semibold text-muted/80">My Projects</h3>
-          <Button variant="ghost" contentAlign="left" size="sm">
-            <SquareAsterisk className="h-5 w-5"/>
+          <h3 className="mb-1 mt-3 px-2 font-semibold text-muted/80">My Projects</h3>
+          <AppButton
+            variant="ghost"
+            size="sm"
+            className="justify-start font-light hover:bg-primary/5"
+          >
+            <SquareAsterisk className="h-4 w-4 text-muted/80" />
             Work
-          </Button>
-          <Button variant="ghost" contentAlign="left" size="sm">
-            <SquareAsterisk className="h-5 w-5"/>
+          </AppButton>
+          <AppButton
+            variant="ghost"
+            size="sm"
+            className="justify-start font-light hover:bg-primary/5"
+          >
+            <SquareAsterisk className="h-4 w-4 text-muted/80" />
             School
-          </Button>
-          <Button variant="ghost" contentAlign="left" size="sm">
-            <SquareAsterisk className="h-5 w-5"/>
+          </AppButton>
+          <AppButton
+            variant="ghost"
+            size="sm"
+            className="justify-start font-light hover:bg-primary/5"
+          >
+            <SquareAsterisk className="h-4 w-4 text-muted/80" />
             Free Time
-          </Button>
-          <Button variant="ghost" contentAlign="left" size="sm">
-            <SquareAsterisk className="h-5 w-5"/>
+          </AppButton>
+          <AppButton
+            variant="ghost"
+            size="sm"
+            className="justify-start font-light hover:bg-primary/5"
+          >
+            <SquareAsterisk className="h-4 w-4 text-muted/80" />
             Code
-          </Button>
+          </AppButton>
         </nav>
 
         {/* Main content  */}
