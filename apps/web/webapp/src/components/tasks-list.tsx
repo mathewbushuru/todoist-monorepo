26dfe5daba991d3557e15cf8ccb892f68afb524f
mathew bushuru <bushuruwafulamathew@gmail.com>
2024-01-06 19:24:41 -0800

    app:webapp - skeleton tasks list

--- apps/web/webapp/src/components/tasks-list.tsx
+++ apps/web/webapp/src/components/tasks-list.tsx
@@ -1,3 +1,59 @@
+interface TaskType {
+  id: number;
+  name: string;
+  description: string | null;
+  dueDate: Date | null;
+}
+
+const sampleTasks: TaskType[] = [
+  {
+    id: 1,
+    name: "Webapp to watch any movie - movies.mathewbushuru.com",
+    description: "Name it Flix?",
+    dueDate: null,
+  },
+  {
+    id: 2,
+    name: "Rix entertainment app - record past movies, books and video games",
+    description: "Add ML recommendations",
+    dueDate: null,
+  },
+  {
+    id: 3,
+    name: "Rebuild portfolio - mathewbushuru.com",
+    description: null,
+    dueDate: null,
+  },
+  {
+    id: 4,
+    name: "Point of Sales retail system SAAS",
+    description: "merchandising, reports, ordering, warehouse, ecommerce API",
+    dueDate: null,
+  },
+  {
+    id: 5,
+    name: "Outfits app",
+    description: null,
+    dueDate: null,
+  },
+  {
+    id: 6,
+    name: "Test task name",
+    description: "Test task description",
+    dueDate: new Date(),
+  },
+];
+
 export default function TasksList() {
-    return <>Tasks List</>
+  return (
+    <div className="px-10">
+      {sampleTasks.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
+    </div>
+  );
+}
+
+function Task({ task }: { task: TaskType }) {
+  return <div className="">{task.name}</div>;
 }
